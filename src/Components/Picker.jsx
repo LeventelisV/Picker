d5c747376eb7adafdb0970b7ab2c1f32712882ad
LeventelisV <61799850+LeventelisV@users.noreply.github.com>
2023-02-16 14:36:21 +0100

    initial displayed data and initial positioning

--- src/Components/Picker.jsx
+++ src/Components/Picker.jsx
@@ -1,18 +1,30 @@
-import { getAllByAltText } from "@testing-library/dom"
 import {PickerContainer,ElementCell} from "./Picker.styles"
 
-export default function Picker({data = [1,2,3,4,5,6,7,8,9],blockHeight = 40,blocks = 7}){
+export default function Picker({data = [1,2,3,4,5,6,7,8,9],blockHeight = 40,blocks = 5}){
     const height = blockHeight * blocks
+    const middlePosition= blockHeight * (Math.floor(blocks/2))
+    const initialSelectedElementPosition = Math.floor(data.length/2)
+    const initialDisplayedData = data.map((element,index)=>{
+        const position = (index - initialSelectedElementPosition)* blockHeight + middlePosition
+        const selected = position === middlePosition
+        return {value: element, position: position, selected: selected}
+    })
+
     return(
         <>
+                {console.log(initialDisplayedData)}
+
 <PickerContainer height={height} >
-    {data.map((element,index)=>
-        <ElementCell 
-            height ={blockHeight} 
-            key={index}
-            position={blockHeight*(index)}
-            >{element}
-        </ElementCell>
+    {initialDisplayedData.map((element,index)=>{
+        // console.log(index)
+         return       <ElementCell 
+                height ={blockHeight} 
+                key={index}
+                position={element.position}
+                >{element.value}
+            </ElementCell>
+    }
+
     )}
 </PickerContainer>
         </>
