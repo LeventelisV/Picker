bbb0ce0af12dc450309d352a0d69c271e5e77b49
LeventelisV <61799850+LeventelisV@users.noreply.github.com>
2023-02-23 13:43:50 +0100

    move the list up and down with keys

--- src/Components/Picker.jsx
+++ src/Components/Picker.jsx
@@ -7,7 +7,7 @@ export default function Picker({data = [1,2,3,4,5,6,7,8,9],blockHeight = 40,bloc
     const listRef = useRef(null)
     const parentListRef = useRef(null)
     
-    const pixelsIWantToMove = 120
+    const pixelsIWantToMove = 40
     const height = blockHeight * blocks
     const middlePosition= blockHeight * (Math.floor(blocks/2))
     const initialSelectedElementPosition = Math.floor(data.length/2)
@@ -23,17 +23,33 @@ export default function Picker({data = [1,2,3,4,5,6,7,8,9],blockHeight = 40,bloc
 
     },[])
 
-    const moveTheListUp = () =>{
+    const moveTheListDown = () =>{
         const parentListPosition = parentListRef.current.getBoundingClientRect().top
-        const listPosition = listRef.current.getBoundingClientRect().top -1 ;
-        if(listPosition + pixelsIWantToMove - parentListPosition  <= middlePosition + Math.abs(displayedData[0].initialPosition)){
-                listRef.current.style.transform = `translateY(${listPosition - parentListPosition + pixelsIWantToMove}px)`
+        const listTopPosition = listRef.current.getBoundingClientRect().top -1 ;
+        console.log(listTopPosition - parentListPosition , Math.abs(listTopPosition - parentListPosition))
+
+        if(listTopPosition + pixelsIWantToMove - parentListPosition  <= middlePosition + Math.abs(displayedData[0].initialPosition)){
+                listRef.current.style.transform = `translateY(${listTopPosition - parentListPosition + pixelsIWantToMove}px)`
         }
-        else if((listPosition  - parentListPosition < middlePosition + Math.abs(displayedData[0].initialPosition))) 
+        else if((listTopPosition  - parentListPosition < middlePosition + Math.abs(displayedData[0].initialPosition))) 
         {
-            const availableMovingSpace = listPosition - (middlePosition + Math.abs(displayedData[0].initialPosition))
-            listRef.current.style.transform = `translateY(${listPosition - parentListPosition + availableMovingSpace}px)`
+            const availableMovingSpace = listTopPosition - (middlePosition + Math.abs(displayedData[0].initialPosition))
+            listRef.current.style.transform = `translateY(${listTopPosition - parentListPosition + availableMovingSpace}px)`
         }
+    }
+
+    const moveTheListUp = () => {
+        const parentListTopPosition = parentListRef.current.getBoundingClientRect().top
+        const parentListBottomPosition = parentListRef.current.getBoundingClientRect().bottom
+        const listBottomPosition = listRef.current.getBoundingClientRect().bottom
+        const listTopPosition = listRef.current.getBoundingClientRect().top -1 ;
+
+        if( ((Math.abs(listTopPosition - parentListTopPosition) === (Math.abs(displayedData.slice(-1)[0].initialPosition)- middlePosition ))  &&  
+        (listTopPosition - parentListTopPosition > 0)) || 
+        Math.abs(listTopPosition - parentListTopPosition) < (Math.abs(displayedData.slice(-1)[0].initialPosition)- middlePosition )){
+            listRef.current.style.transform = `translateY(${listTopPosition - parentListTopPosition - pixelsIWantToMove}px)`
+        }
+
     }
 
     return(
@@ -52,9 +68,16 @@ export default function Picker({data = [1,2,3,4,5,6,7,8,9],blockHeight = 40,bloc
 
     )}</div>
 </PickerContainer>
+<div>
 <button 
-    className="bg-red-500 h-20 w-40 rounded-lg"
-    onClick={moveTheListUp}>moveUp</button>
+    className="bg-red-500 h-20 w-40 rounded-lg mr-5"
+    onClick={moveTheListUp}>moveUp
+</button>
+<button 
+    className="bg-green-500 h-20 w-40 rounded-lg"
+    onClick={moveTheListDown}>moveDown
+</button>
+</div>
 </div>
         </>
     )
