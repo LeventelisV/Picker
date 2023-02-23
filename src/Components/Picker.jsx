7c42897df801d0d6becf91632af5a787a9ee73b1
LeventelisV <61799850+LeventelisV@users.noreply.github.com>
2023-02-23 10:21:21 +0100

    refactor code

--- src/Components/Picker.jsx
+++ src/Components/Picker.jsx
@@ -3,13 +3,11 @@ import {PickerContainer,ElementCell} from "./Picker.styles"
 
 export default function Picker({data = [1,2,3,4,5,6,7,8,9],blockHeight = 40,blocks = 5}){
     console.log("Picker")
-    const mainListRef = useRef(null)
-    const parentRef = useRef(null)
-    const parentPosition = useRef(null)
-    const [render,setRender] = useState(false)
-    const pixelsIWantToMove = 120
-   
     
+    const listRef = useRef(null)
+    const parentListRef = useRef(null)
+    
+    const pixelsIWantToMove = 120
     const height = blockHeight * blocks
     const middlePosition= blockHeight * (Math.floor(blocks/2))
     const initialSelectedElementPosition = Math.floor(data.length/2)
@@ -22,45 +20,28 @@ export default function Picker({data = [1,2,3,4,5,6,7,8,9],blockHeight = 40,bloc
     const [displayedData,setDisplayedData] = useState(initialDisplayedData)
 
     useEffect(()=>{
-       parentPosition.current =  parentRef.current.getBoundingClientRect().top
+
     },[])
 
     const moveTheListUp = () =>{
-        let childPosition = mainListRef.current.getBoundingClientRect().top -1 ;
-        console.log(childPosition,"clildPosition")
-        console.log(parentPosition.current,"ParentPosition")
-        console.log(middlePosition,"middlePosition")
-        console.log(middlePosition - (childPosition - parentPosition.current - 1),"middlePosition - (childPosition - parentPosition.current - 1)")
-        console.log(displayedData[0].initialPosition,"displayedData[0].initialPosition")
-        if(childPosition + pixelsIWantToMove - parentPosition.current  <= middlePosition + Math.abs(displayedData[0].initialPosition)){console.log("case1");
-                mainListRef.current.style.transform = `translateY(${childPosition - parentPosition.current + pixelsIWantToMove}px)`
+        const parentListPosition = parentListRef.current.getBoundingClientRect().top
+        const listPosition = listRef.current.getBoundingClientRect().top -1 ;
+        if(listPosition + pixelsIWantToMove - parentListPosition  <= middlePosition + Math.abs(displayedData[0].initialPosition)){
+                listRef.current.style.transform = `translateY(${listPosition - parentListPosition + pixelsIWantToMove}px)`
         }
-        else if((childPosition  - parentPosition.current < middlePosition + Math.abs(displayedData[0].initialPosition))) 
-        // && (childPosition <= middlePosition + Math.abs(displayedData[0].initialPosition)))
+        else if((listPosition  - parentListPosition < middlePosition + Math.abs(displayedData[0].initialPosition))) 
         {
-            console.log(childPosition,"-----", middlePosition + Math.abs(displayedData[0].initialPosition))
-
-            const availableMovingSpace = childPosition - (middlePosition + Math.abs(displayedData[0].initialPosition))
-            mainListRef.current.style.transform = `translateY(${childPosition - parentPosition.current + availableMovingSpace}px)`
-
+            const availableMovingSpace = listPosition - (middlePosition + Math.abs(displayedData[0].initialPosition))
+            listRef.current.style.transform = `translateY(${listPosition - parentListPosition + availableMovingSpace}px)`
         }
-            // else{
-            //     const availableMovingSpace =  middlePosition + Math.abs(displayedData[0].initialPosition) - childPosition;
-            //     console.log(availableMovingSpace,"availableMovingSpace")
-            //     console.log(middlePosition + Math.abs(displayedData[0].initialPosition),"middlePosition + Math.abs(displayedData[0].initialPosition)")
-            //     console.log(childPosition,"childPosition")
-            //     // mainListRef.current.style.transform = `translateY(${childPosition - parentPosition.current + availableMovingSpace}px)`
-            // }
-        setRender(!render)
     }
 
     return(
         <>
         <div className="flex justify-between m-20 mr-full">
-                {/* {console.log(displayedData)} */}
 
-<PickerContainer height={height} ref={parentRef}>{console.log(displayedData)}
-    <div ref={mainListRef}>{displayedData.map((element,index)=>{
+<PickerContainer height={height} ref={parentListRef}>
+    <div ref={listRef}>{displayedData.map((element,index)=>{
          return       <ElementCell 
                 height ={blockHeight} 
                 key={index}
